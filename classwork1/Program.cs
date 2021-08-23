5994bb891c733e7b45ea6c1c8811058c3fde1e77
csujaan <sujaan1997@gmail.com>
2021-08-23 16:02:24 +0530

    program

---
+++ classwork1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1,  f = 1;
+            while (f < 6)
+            {
+                fact = fact * f;
+                f++;
+            }
+            System.Console.WriteLine("The factorial of 6 is " + fact);
+
+            int a = 0, b = 1,c;
+            System.Console.Write(a + ", ");
+            while (b < 20)
+            {
+                System.Console.Write(b + ", ");
+                c = b + a;
+                a = b;
+                b = c;
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
